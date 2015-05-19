04a5e558618f1318d9c39bbedf2c85476c366c14
Kim Alvefur <zash@zash.se>
2015-05-19 18:34:08 +0200

    mod_storage_(archive-capable): Change order of arguments to :append to be the same as return values from :find iterator (see prosody 41725f3df3cc)

--- mod_storage_xmlarchive/mod_storage_xmlarchive.lua
+++ mod_storage_xmlarchive/mod_storage_xmlarchive.lua
@@ -23,7 +23,10 @@ end);
 local archive = {};
 local archive_mt = { __index = archive };
 
-function archive:append(username, _, when, with, data)
+function archive:append(username, _, data, when, with)
+	if type(when) ~= "number" then
+		value, when, with = when, with, value;
+	end
 	if getmetatable(data) ~= st.stanza_mt then
 		module:log("error", "Attempt to store non-stanza object, traceback: %s", debug.traceback());
 		return nil, "unsupported-datatype";
