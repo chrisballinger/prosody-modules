06c7ad8ca21f4b25310899fa567deba145a09784
Kim Alvefur <zash@zash.se>
2015-09-04 00:23:46 +0200

    mod_http_index: Move template out into a file and make it configurable

--- mod_http_index/mod_http_index.lua
+++ mod_http_index/mod_http_index.lua
@@ -3,55 +3,13 @@ local render = require"util.interpolation".new("%b{}", require"util.stanza".xml_
 
 module:depends"http";
 
--- TODO Move templates into files
-local base_template = [[
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="generator" value="prosody/{prosody_version} mod_{mod_name}">
-<link rel="canonical" href="{canonical}">
-<title>{title}</title>
-<style>
-body{background-color:#eeeeec;margin:1ex 0;padding-bottom:3em;font-family:Arial,Helvetica,sans-serif;}
-header,footer{margin:1ex 1em;}
-footer{font-size:smaller;color:#babdb6;}
-.content{background-color:white;padding:1em;list-style-position:inside;}
-nav{font-size:large;margin:1ex 1ex;clear:both;line-height:1.5em;}
-nav a{padding: 1ex;text-decoration:none;}
-@media screen and (min-width: 460px) {
-nav{font-size:x-large;margin:1ex 1em;}
-}
-a:link,a:visited{color:#2e3436;text-decoration:none;}
-a:link:hover,a:visited:hover{color:#3465a4;}
-ul{padding:0;}
-li{list-style:none;}
-hr{visibility:hidden;clear:both;}
-br{clear:both;}
-li:hover time{opacity:1;}
-</style>
-</head>
-<body>
-<header>
-<h1>{title}</h1>
-</header>
-<hr>
-<div class="content">
-<nav>
-<ul>{items#
-<li><a href="{item.url}" title="{item.module}">{item.name}</a></li>}
-</ul>
-</nav>
-</div>
-<hr>
-<footer>
-<br>
-<div class="powered-by">Prosody {prosody_version?}</div>
-</footer>
-</body>
-</html>
-]];
+local base_template;
+do
+	local template_file = module:get_option_string(module.name .. "_template", module.name .. ".html");
+	template_file = assert(module:load_resource(template_file));
+	base_template = template_file:read("*a");
+	template_file:close();
+end
 
 local canonical = module:http_url(nil, "/");
 
