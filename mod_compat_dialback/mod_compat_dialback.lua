6dbad7434781a92fa49a3f551bd5bb65645a0fcf
Kim Alvefur <zash@zash.se>
2015-10-11 15:39:23 +0200

    mod_compat_dialback: Workaround for Dialback with servers that violate RFC 6120 ? 4.7.2

---
+++ mod_compat_dialback/mod_compat_dialback.lua
@@ -0,0 +1,15 @@
+-- Prosody IM
+-- Copyright (C) 2008-2010 Matthew Wild
+-- Copyright (C) 2008-2010 Waqas Hussain
+--
+-- This project is MIT/X11 licensed. Please see the
+-- COPYING file in the source package for more information.
+--
+
+module:set_global();
+
+module:global("s2s-stream-features-legacy", function (data)
+	if data.origin.type == "s2sin_unauthed" then
+		data.features:tag("dialback", { xmlns='urn:xmpp:features:dialback' }):up();
+	end
+end);
