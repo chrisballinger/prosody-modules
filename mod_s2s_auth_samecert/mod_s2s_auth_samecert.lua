50b1eb9b93df6f697ef39ecde110abdbf2075bb8
Kim Alvefur <zash@zash.se>
2016-07-08 22:37:48 +0200

    mod_s2s_auth_samecert: Log which s2sout has a matching cert

--- mod_s2s_auth_samecert/mod_s2s_auth_samecert.lua
+++ mod_s2s_auth_samecert/mod_s2s_auth_samecert.lua
@@ -8,6 +8,7 @@ module:hook("s2s-check-certificate", function(event)
 	
 	local outgoing = hosts[session.to_host].s2sout[session.from_host];
 	if outgoing and outgoing.type == "s2sout" and outgoing.secure and outgoing.conn:socket():getpeercertificate():pem() == cert:pem() then
+		session.log("debug", "Certificate matches that of s2sout%s", tostring(outgoing):match("[a-f0-9]+$"));
 		session.cert_identity_status = outgoing.cert_identity_status;
 		session.cert_chain_status = outgoing.cert_chain_status;
 		return true;
