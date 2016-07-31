408f775a7920ec3205dd72022ddc8209530df41e
Kim Alvefur <zash@zash.se>
2016-07-31 17:59:13 +0200

    mod_track_muc_joins: Keep the nickname around

--- mod_track_muc_joins/mod_track_muc_joins.lua
+++ mod_track_muc_joins/mod_track_muc_joins.lua
@@ -1,4 +1,5 @@
 local jid_bare = require "util.jid".bare;
+local jid_split = require "util.jid".split;
 local sessions = prosody.full_sessions;
 
 module:hook("presence/full", function (event)
@@ -12,9 +13,10 @@ module:hook("presence/full", function (event)
 
 	local from_jid = stanza.attr.from;
 	local room = jid_bare(from_jid);
+	local nick = jid_split(from_jid);
 	local joined = stanza.attr.type;
 	if joined == nil then
-		joined = true;
+		joined = nick;
 	elseif joined == "unavailable" then
 		joined = nil;
 	else
