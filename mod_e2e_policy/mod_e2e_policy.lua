7a15ce7984b84958d185f466ed92859f1045ac3f
Michel Le Bihan <michel@lebihan.pl>
2016-10-13 16:53:40 +0200

    mod_e2e_policy: Do not reply to error stenzas

--- mod_e2e_policy/mod_e2e_policy.lua
+++ mod_e2e_policy/mod_e2e_policy.lua
@@ -10,12 +10,13 @@ local e2e_policy_message_optional_muc = module:get_option_string("e2e_policy_mes
 local e2e_policy_message_required_muc = module:get_option_string("e2e_policy_message_required_muc", "For security reasons, OMEMO, OTR or PGP encryption is required for MUC on this server.");
 
 function warn_on_plaintext_messages(event)
-    if e2e_policy_whitelist:contains(event.stanza.attr.from) or e2e_policy_whitelist:contains(event.stanza.attr.to) then -- check if JID is whitelisted
+    -- check if JID is whitelisted
+    if e2e_policy_whitelist:contains(event.stanza.attr.from) or e2e_policy_whitelist:contains(event.stanza.attr.to) then
         return nil;
     end
     local body = event.stanza:get_child_text("body");
     -- do not warn for status messages
-    if not body then
+    if not body or event.stanza.attr.type == "error" then
         return nil;
     end
     -- check otr
@@ -30,7 +31,7 @@ function warn_on_plaintext_messages(event)
     if event.stanza:get_child("x", "jabber:x:encrypted") then
         return nil;
     end
-	-- check xep373 pgp (OX) https://xmpp.org/extensions/xep-0373.html
+    -- check xep373 pgp (OX) https://xmpp.org/extensions/xep-0373.html
     if event.stanza:get_child("openpgp", "urn:xmpp:openpgp:0") then
         return nil;
     end
