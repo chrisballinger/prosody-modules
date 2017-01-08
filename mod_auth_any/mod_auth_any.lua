2aed8872b2eafd08ec0b25d3ffb932807ee3875f
Kim Alvefur <zash@zash.se>
2017-01-08 11:27:01 +0100

    mod_auth_any: Remove provider constructor, it's only called once anyways

--- mod_auth_any/mod_auth_any.lua
+++ mod_auth_any/mod_auth_any.lua
@@ -22,46 +22,42 @@ local hosts = hosts;
 
 local prosody = _G.prosody;
 
-function new_default_provider(host)
-	local provider = { name = "any" };
-	log("debug", "initializing default authentication provider for host '%s'", host);
+local host = module.host;
+local provider = { name = "any" };
 
-	function provider.test_password(username, password)
-		return true;
-	end
-
-	function provider.set_password(username, password)
-		local account = datamanager.load(username, host, "accounts");
-		if account then
-			account.password = password;
-			return datamanager.store(username, host, "accounts", account);
-		end
-		return nil, "Account not available.";
-	end
-
-	function provider.user_exists(username)
-		return true;
-	end
-
-	function provider.create_user(username, password)
-		return datamanager.store(username, host, "accounts", {password = password});
-	end
-
-	function provider.delete_user(username)
-		return datamanager.store(username, host, "accounts", nil);
-	end
-
-	function provider.get_sasl_handler()
-		local getpass_authentication_profile = {
-			plain_test = function(sasl, username, password, realm)
-				return true, true;
-			end
-		};
-		return new_sasl(module.host, getpass_authentication_profile);
-	end
-
-	return provider;
+function provider.test_password(username, password)
+	return true;
 end
 
-module:add_item("auth-provider", new_default_provider(module.host));
+function provider.set_password(username, password)
+	local account = datamanager.load(username, host, "accounts");
+	if account then
+		account.password = password;
+		return datamanager.store(username, host, "accounts", account);
+	end
+	return nil, "Account not available.";
+end
+
+function provider.user_exists(username)
+	return true;
+end
+
+function provider.create_user(username, password)
+	return datamanager.store(username, host, "accounts", {password = password});
+end
+
+function provider.delete_user(username)
+	return datamanager.store(username, host, "accounts", nil);
+end
+
+function provider.get_sasl_handler()
+	local getpass_authentication_profile = {
+		plain_test = function(sasl, username, password, realm)
+			return true, true;
+		end
+	};
+	return new_sasl(module.host, getpass_authentication_profile);
+end
+
+module:add_item("auth-provider", provider);
 
