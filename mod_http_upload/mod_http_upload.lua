1d34480cc8f62882fa3af23d2a830a747597c778
Kim Alvefur <zash@zash.se>
2017-01-29 18:12:43 +0100

    mod_http_upload: Change join_path to match behaviour of util.paths in 0.10

--- mod_http_upload/mod_http_upload.lua
+++ mod_http_upload/mod_http_upload.lua
@@ -18,8 +18,8 @@ local t_concat = table.concat;
 local t_insert = table.insert;
 local s_upper = string.upper;
 
-local function join_path(a, b)
-return a .. package.config:sub(1,1) .. b;
+local function join_path(...)
+	table.concat({ ... }, package.config:sub(1,1));
 end
 
 -- config
