918838129ffa3f0488623e8534fb900bf83358a2
Kim Alvefur <zash@zash.se>
2017-02-20 00:06:28 +0100

    mod_mam: Add annotations [luacheck]

--- mod_mam/mamprefs.lib.lua
+++ mod_mam/mamprefs.lib.lua
@@ -2,10 +2,12 @@
 -- Copyright (C) 2011-2013 Kim Alvefur
 --
 -- This file is MIT/X11 licensed.
+-- luacheck: ignore 122/prosody
 
 local global_default_policy = module:get_option("default_archive_policy", true);
 
 do
+	-- luacheck: ignore 211/prefs_format
 	local prefs_format = {
 		[false] = "roster",
 		-- default ::= true | false | "roster"
@@ -15,7 +17,7 @@ do
 	};
 end
 
-local sessions = hosts[module.host].sessions;
+local sessions = prosody.hosts[module.host].sessions;
 local prefs = module:open_store("archive2_prefs");
 
 local function get_prefs(user)
