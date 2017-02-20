c69546fe2b0b1f5ae8bdf3670c1c1824865ff207
Matthew Wild <mwild1@gmail.com>
2017-02-20 13:12:51 +0000

    mod_firewall: Fix SCAN to pass correct variable to scan_list()

--- mod_firewall/conditions.lib.lua
+++ mod_firewall/conditions.lib.lua
@@ -275,7 +275,7 @@ function condition_handlers.SCAN(scan_expression)
 	if not (search_name) then
 		error("Error parsing SCAN expression, syntax: SEARCH for PATTERN in LIST");
 	end
-	return ("scan_list(%s, %s)"):format(list_name, "tokens_"..search_name.."_"..pattern_name), { "scan_list", "tokens:"..search_name.."_"..pattern_name, "list:"..list_name };
+	return ("scan_list(list_%s, %s)"):format(list_name, "tokens_"..search_name.."_"..pattern_name), { "scan_list", "tokens:"..search_name.."_"..pattern_name, "list:"..list_name };
 end
 
 return condition_handlers;
