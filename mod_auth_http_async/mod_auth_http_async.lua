8fafad0e9fd1252b0a01d57f251600c0013287a6
JC Brand <jc@opkode.com>
2017-03-21 09:14:03 +0000

    mod_auth_http_async: For sync calls, use LuaSockets' HTTP lib

--- mod_auth_http_async/mod_auth_http_async.lua
+++ mod_auth_http_async/mod_auth_http_async.lua
@@ -8,9 +8,10 @@
 --
 
 local new_sasl = require "util.sasl".new;
+local http = require "socket.http";
+local https = require "ssl.https";
 local base64 = require "util.encodings".base64.encode;
 local have_async, async = pcall(require, "util.async");
-local http = require "net.http";
 
 local log = module._log;
 local host = module.host;
@@ -18,47 +19,71 @@ local host = module.host;
 local api_base = module:get_option_string("http_auth_url",  ""):gsub("$host", host);
 if api_base == "" then error("http_auth_url required") end
 
-local function async_http_request(url, ex)
+local provider = {};
+
+-- globals required by socket.http
+if rawget(_G, "PROXY") == nil then
+	rawset(_G, "PROXY", false)
+end
+if rawget(_G, "base_parsed") == nil then
+	rawset(_G, "base_parsed", false)
+end
+
+local function async_http_auth(url, username, password)
 	local wait, done = async.waiter();
 	local content, code, request, response;
+	local ex = {
+		headers = { Authorization = "Basic "..base64(username..":"..password); };
+	}
 	local function cb(content_, code_, request_, response_)
 		content, code, request, response = content_, code_, request_, response_;
 		done();
 	end
 	http.request(url, ex, cb);
 	wait();
-	return content, code, request, response;
-end
-
-local provider = {};
-
-function provider.test_password(username, password)
-	local url = api_base:gsub("$user", username);
-	log("debug", "Testing password for user %s at host %s with URL %s", username, host, url);
-	local ex = {
-		headers = { Authorization = "Basic "..base64(username..":"..password); };
-	}
-	if (have_async) then
-	    local _, code = async_http_request(url, ex);
-	    if code >= 200 and code <= 299 then
-			module:log("debug", "HTTP auth provider confirmed valid password");
-	        return true;
-	    else
-	        module:log("debug", "HTTP auth provider returned status code %d", code);
-	    end
+	if code >= 200 and code <= 299 then
+		module:log("debug", "HTTP auth provider confirmed valid password");
+		return true;
 	else
-	    local ok, err = http.request(url, ex, function(body, code)
-			if code >= 200 and code <= 299 then
-				module:log("debug", "HTTP auth provider confirmed valid password");
-			else
-				module:log("debug", "HTTP auth provider returned status code %d", code);
-			end
-		end);
-	    if ok then
-	        return true;
-	    end
+		module:log("debug", "HTTP auth provider returned status code %d", code);
 	end
 	return nil, "Auth failed. Invalid username or password.";
+end
+
+local function sync_http_auth(url)
+	local request;
+	if string.sub(url, 1, string.len('https')) == 'https' then
+		request = https.request;
+	else
+		request = http.request;
+	end
+	local _, code, headers, status = request{
+		url = url,
+		headers = { ACCEPT = "application/json, text/plain, */*"; }
+	};
+	if type(code) == "number" and code >= 200 and code <= 299 then
+		module:log("debug", "HTTP auth provider confirmed valid password");
+		return true;
+	else
+		module:log("debug", "HTTP auth provider returned status code: "..code);
+	end
+	return nil, "Auth failed. Invalid username or password.";
+end
+
+function provider.test_password(username, password)
+	local url = api_base:gsub("$user", username):gsub("$password", password);
+	log("debug", "Testing password for user %s at host %s with URL %s", username, host, url);
+	if (have_async) then
+		return async_http_auth(url, username, password);
+	else
+		return sync_http_auth(url);
+	end
+end
+
+function provider.users()
+	return function()
+		return nil;
+	end
 end
 
 function provider.set_password(username, password)
@@ -84,5 +109,5 @@ function provider.get_sasl_handler()
 		end
 	});
 end
-	
+
 module:provides("auth", provider);
